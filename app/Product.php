37aab17b2dd78f1c14ec1fb3ce20b890a06d24a4
Ahmedgadallaah <ahmedgadallah1899@gmail.com>
2019-12-22 18:01:36 +0200

    eshopper

---
+++ app/Product.php
@@ -0,0 +1,26 @@
+<?php
+    namespace App;
+
+    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\SoftDeletes;
+
+    class Product extends Model
+    {
+        use SoftDeletes;
+
+        protected $fillable = [
+            'name', 'price', 'units', 'description', 'image'
+        ];
+
+        public function orders(){
+            return $this->hasMany(Order::class);
+        }
+    }
+// namespace App;
+
+// use Illuminate\Database\Eloquent\Model;
+
+// class Product extends Model
+// {
+//     //
+// } -->
